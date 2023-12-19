491678f7d63165087cfa29973272ee8cf265a685
Romain GILOT <romaingilot@MacBook-Air-de-Romain.local>
2023-12-19 13:49:44 +0100

    front Pool

---
+++ eurofoot2024/src/components/PoolCard.js
@@ -0,0 +1,30 @@
+export function PoolCard({title, teams}) {
+    return(
+        <div className="m-10">
+            <div className="w-80 text-yellow-500 text-2xl bg-[#002B92] p-3 font-extrabold text-center">{title}</div>
+            <div className="border border-1 border-blue-600 flex flex-col p-3 ">
+                <div className="flex items-center mb-4">
+                    <div className="h-8 w-8 rounded-full bg-white"></div>
+                    <div className="text-white ml-4 font-semibold">{teams}</div>
+                </div>
+                <div className="flex items-center mb-4">
+                    <div className="h-8 w-8 rounded-full bg-white"></div>
+                    <div className="text-white ml-4 font-semibold">{teams}</div>
+                </div>
+                <div className="flex items-center mb-4">
+                    <div className="h-8 w-8 rounded-full bg-white"></div>
+                    <div className="text-white ml-4 font-semibold">{teams}</div>
+                </div>
+                <div className="flex items-center mb-4">
+                    <div className="h-8 w-8 rounded-full bg-white"></div>
+                    <div className="text-white ml-4 font-semibold">{teams}</div>
+                </div>
+                <div className="flex items-center mb-4">
+                    <div className="h-8 w-8 rounded-full bg-white"></div>
+                    <div className="text-white ml-4 font-semibold">{teams}</div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ eurofoot2024/src/pages/Pool/Pool.js
@@ -0,0 +1,30 @@
+import { PoolCard } from "../../components/PoolCard";
+import logoEuro from "../../images/logo.png"
+import footerImage from "../../images/bg.png"
+import { ClickIcon } from "../../components/icons/ClickIcon";
+
+export function Pool() {
+    return(
+        <div className="bg-[#143DDB] h-full">
+            <div className="flex items-center justify-center">
+                <img src={logoEuro} className="h-60" alt="logo euro"/>
+                <div className="text-center ml-5">
+                    <div className="text-5xl text-white font-bold tracking-wides">CHAPEAUX</div>
+                    <div className="text-7xl text-white font-bold tracking-widest">TIRAGE</div>
+                </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-center">
+                <PoolCard title="POT 1" teams="France"/>
+                <PoolCard title="POT 2" teams="France"/>
+                <PoolCard title="POT 3" teams="France"/>
+                <PoolCard title="POT 4" teams="France"/>
+            </div>
+            <div className="bg-cover flex items-center justify-center" style={{ backgroundImage: `url(${footerImage})` , height: 317}}>
+                <button className="p-3 bg-blue-500 rounded-lg text-gray-100 font-semibold text-lg flex">
+                    <ClickIcon/>
+                   <span className="ml-4">Générer le tirage au sort</span>
+                </button>
+            </div>
+        </div>
+    )
+}
