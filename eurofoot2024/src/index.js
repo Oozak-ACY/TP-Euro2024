6e2eed49564fdf8381a17c22dbafc59f56998e3b
Loan KEOVILAY <loankeovilay@gmail.com>
2023-12-19 16:07:51 +0100

    ajout de la route 'tirage'

--- eurofoot2024/src/index.js
+++ eurofoot2024/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './input.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Tirage } from './Tirage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/tirage" element={<Tirage />} />
+    </Routes>
+  </BrowserRouter>
 );
+
+
 
 
 
