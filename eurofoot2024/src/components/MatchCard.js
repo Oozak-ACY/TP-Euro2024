df7e3b9cc3d7ac9c336266ad3deae785ff30432d
Loan KEOVILAY <loankeovilay@gmail.com>
2023-12-21 13:19:26 +0100

    Mise en place de la génération des matchs

--- eurofoot2024/src/components/MatchCard.js
+++ eurofoot2024/src/components/MatchCard.js
@@ -1,49 +1,40 @@
-export function MatchCard({ title, team1, team2 }) {
+import React from "react";
+const importAll = (r) => r.keys().map(r);
+const images = importAll(
+  require.context("../images/flags/4x3/", false, /\.(png|jpe?g|svg)$/)
+);
+
+export function MatchCard({ title, matchs, equipes}) {
   return (
     <div className="m-10">
       <div className="w-80 text-yellow-500 text-2xl bg-[#002B92] p-3 font-extrabold text-center">
         {title}
       </div>
       <div className="border border-[#002B92]">
-        <div className="flex just flex-col p-3 ">
-          <div className="flex justify-between pl-10 pr-10">
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team1}</div>
+        {matchs.map((match, index) => {
+          const equipe1Image = images.find((image) =>
+          image.includes(`/${match.code_equipe1}.`)
+          );
+          const equipe2Image = images.find((image) =>
+          image.includes(`/${match.code_equipe2}.`)
+          );
+        
+          return(
+            <div key={index} className="flex flex-col p-3">
+              <div className="flex justify-between pl-10 pr-10">
+                <div className="flex flex-col items-center mb-4">
+                  <div className="h-8 w-8 bg-cover bg-center  rounded-full " style={{ backgroundImage: `url(${equipe1Image})` }}></div>
+                  <div className="text-white">{match["nom_equipe1"]}</div>
+                </div>
+                <span className="text-white">VS</span>
+                <div className="flex flex-col items-center mb-4">
+                  <div className="h-8 w-8 bg-cover bg-center  rounded-full " style={{ backgroundImage: `url(${equipe2Image})` }}></div>
+                  <div className="text-white">{match["nom_equipe2"]}</div>
+                </div>
+              </div>
             </div>
-            <span className="text-white">VS</span>
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team2}</div>
-            </div>
-          </div>
-        </div>
-        <div className="flex just flex-col p-3 ">
-          <div className="flex justify-between pl-10 pr-10">
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team1}</div>
-            </div>
-            <span className="text-white">VS</span>
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team2}</div>
-            </div>
-          </div>
-        </div>
-        <div className="flex just flex-col p-3 ">
-          <div className="flex justify-between pl-10 pr-10">
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team1}</div>
-            </div>
-            <span className="text-white">VS</span>
-            <div className="flex flex-col items-center mb-4">
-              <div className="h-8 w-8 rounded-full bg-white"></div>
-              <div className="text-white">{team2}</div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
