57ba3f09764fd2d80c549b36cb2b1ba690b9d4c1
romaingilot <romain.gilot86@outlook.fr>
2023-12-22 12:59:07 +0100

    ajout du backend des statistiques

--- api/db_test.js
+++ api/db_test.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -10,71 +10,116 @@ app.use(cors());
 
 // Configurer la connexion à la base de données
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    multipleStatements: true
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+  multipleStatements: true,
 });
 
 //Api pour récupérer les équipes
-app.get('/api/hat1', (req, res) => {
-    db.query('SELECT * FROM equipes WHERE hat = 1 AND playoff IS NULL', (err, results) => {
-        if (err) throw err;
-        res.json(results);
-    });
+app.get("/api/hat1", (req, res) => {
+  db.query(
+    "SELECT * FROM equipes WHERE hat = 1 AND playoff IS NULL",
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    }
+  );
 });
 
-app.get('/api/hat2', (req, res) => {
-    db.query('SELECT * FROM equipes WHERE hat = 2 ', (err, results) => {
-        if (err) throw err;
-        
-        res.json(results);
-    });
+app.get("/api/hat2", (req, res) => {
+  db.query("SELECT * FROM equipes WHERE hat = 2 ", (err, results) => {
+    if (err) throw err;
+
+    res.json(results);
+  });
 });
 
-app.get('/api/hat3', (req, res) => {
-    db.query('SELECT * FROM equipes WHERE hat = 3', (err, results) => {
-        if (err) throw err;
-        
-        res.json(results);
-    });
+app.get("/api/hat3", (req, res) => {
+  db.query("SELECT * FROM equipes WHERE hat = 3", (err, results) => {
+    if (err) throw err;
+
+    res.json(results);
+  });
 });
 
 // API pour récupérer des données
-app.get('/api/equipes', (req, res) => {
-    db.query('SELECT * FROM equipes', (err, results) => {
-        if (err) throw err;
-        
-        res.json(results);
-    });
+app.get("/api/equipes", (req, res) => {
+  db.query("SELECT * FROM equipes", (err, results) => {
+    if (err) throw err;
+
+    res.json(results);
+  });
 });
 
-app.post('/api/tirage', (req, res) => {
-    if (!req.body.query) {
-        return res.status(400).json({ message: 'Requete manquante' });
-    }
-    const data = req.body["query"];
-    try{
-        let query = "TRUNCATE TABLE matchs;"
-        data.forEach(element => {
-            query += element;
-        });
-        db.query(query, (err) => {
-            if (err) throw err;
-        });
+app.get("/api/matchresults", (req, res) => {
+  db.query("SELECT * FROM matchresults", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
 
-        db.query('SELECT m.code_equipe1, m.code_equipe2, e1.name as nom_equipe1, e2.name as nom_equipe2, m.groupe  FROM matchs m LEFT JOIN equipes e1 on e1.code = m.code_equipe1 LEFT JOIN equipes e2 on e2.code = m.code_equipe2', (err, results) => {
-            if (err) throw err;
-            res.json(results);
-        });
-    }catch (erreur){
-        console.error(erreur);
-        res.json({message: 'Aucune données n\' été transmise à l\'api'});
+app.get("/api/groupstandings", (req, res) => {
+  db.query(
+    `
+    SELECT team, phase, SUM(points) AS totalPoints
+    FROM (
+      SELECT team1 AS team, team1Goals AS goals, phase,
+             CASE 
+               WHEN team1Goals > team2Goals THEN 3
+               WHEN team1Goals = team2Goals THEN 1
+               ELSE 0
+             END AS points
+      FROM matchresults
+      UNION ALL
+      SELECT team2 AS team, team2Goals AS goals, phase,
+             CASE 
+               WHEN team2Goals > team1Goals THEN 3
+               WHEN team2Goals = team1Goals THEN 1
+               ELSE 0
+             END AS points
+      FROM matchresults
+    ) AS combined
+    WHERE phase != "pool"
+    GROUP BY team, phase
+    ORDER BY phase, totalPoints DESC, team;
+  `,
+    (err, results) => {
+      if (err) throw err;
+      res.json(results);
     }
-    
+  );
+});
+
+app.post("/api/tirage", (req, res) => {
+  if (!req.body.query) {
+    return res.status(400).json({ message: "Requete manquante" });
+  }
+  const data = req.body["query"];
+  try {
+    let query = "TRUNCATE TABLE matchs;";
+    data.forEach((element) => {
+      query += element;
+    });
+    db.query(query, (err) => {
+      if (err) throw err;
+    });
+
+    db.query(
+      "SELECT m.code_equipe1, m.code_equipe2, e1.name as nom_equipe1, e2.name as nom_equipe2, m.groupe  FROM matchs m LEFT JOIN equipes e1 on e1.code = m.code_equipe1 LEFT JOIN equipes e2 on e2.code = m.code_equipe2",
+      (err, results) => {
+        if (err) throw err;
+        res.json(results);
+      }
+    );
+  } catch (erreur) {
+    console.error(erreur);
+    res.json({ message: "Aucune données n' été transmise à l'api" });
+  }
 });
 
 app.listen(3001, () => {
-    console.log('Serveur démarré sur le port 3001');
+  console.log("Serveur démarré sur le port 3001");
 });
